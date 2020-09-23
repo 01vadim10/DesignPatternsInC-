1ba06de5f380318ad0e7915a43e17eb4305cdf18
Vadim Zylevich <vzy@leapwork.com>
2020-09-23 23:02:55 +0300

    Init commit

---
+++ BuilderExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Remoting;
+
+namespace BuilderExercise
+{
+    public class CodeBuilder
+    {
+        public ObjectHandle type;
+
+        public CodeBuilder(string className)
+        {
+            type = Activator.CreateInstance(null, className);
+        }
+
+        public CodeBuilder AddField(string name, string type)
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cb = new CodeBuilder("Person").AddField("Name", "string").AddField("Age", "int");
+            Console.WriteLine(cb);
+        }
+    }
+}
